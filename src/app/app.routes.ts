4364e4469ba51eb09d51cb799ad779ce641f8d95
Cagri TURK <haliscagri@gmail.com>
2025-04-14 15:09:14 +0300

    feat(routing): configure service module navigation * Add /service routes * Add redirectTo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { ServiceListComponent } from './components/service-list/service-list.component';
+import { ServiceDetailComponent } from './components/service-detail/service-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+      path: 'services',
+      component: ServiceListComponent,
+      title: 'Services List'
+    },
+    {
+      path: 'services/:id',
+      component: ServiceDetailComponent,
+      title: 'Service Details'
+    },
+    {
+      path: '',
+      redirectTo: '/services',
+      pathMatch: 'full'
+    },
+    {
+      path: '**',
+      redirectTo: '/services'
+    }
+  ];
+  
